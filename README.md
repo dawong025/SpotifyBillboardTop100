080b80bd3127b460575ab77ae2ed10a3ae0b1b58
Darren Wong <dwong19@mail.sfsu.edu>
2024-01-05 16:17:23 -0800

    Added support for dupe handling, decade song playlist

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Spotify Billboard Top 100s
-A program using Python, BeautifulSoup, and the Spotify API via Spotipy to add historical top 100 songs to a Spotify playlist.
+A program using Python, BeautifulSoup, and the Spotify API via Spotipy to rediscover classic hits to a Spotify playlist.
+
+## Todo List
+- Remove duplicates from the list
+- String formatting for artist features
+- format string length to < 100 chars
--- main.py
+++ main.py
@@ -4,36 +4,45 @@ import spotipy
 from spotipy import SpotifyOAuth
 import time
 
-year = "2010"
+year = 2010
 day = "01"
 
 all_artist_names = []
 all_song_names = []
+for year in range(2010, 2020):
+    str_year = str(year)
 
-for month in range(1, 13):
-    # print(month)
-    if len(str(month)) == 1:
-        str_month = "0" + str(month)
-    else:
-        str_month = str(month)
+    for month in range(1, 13):
+        if len(str(month)) == 1:
+            str_month = "0" + str(month)
+        else:
+            str_month = str(month)
 
-    date = year + "-" + str_month + "-" + day
+        date = str_year + "-" + str_month + "-" + day
+        # print(str(date))
+        response = requests.get("https://billboard.com/charts/hot-100/" + date)
+        # print(response.text)
 
-    response = requests.get("https://billboard.com/charts/hot-100/" + date)
-    # print(response.text)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        song_name_spans = soup.select('li ul li h3')
+        song_names = [song.getText().strip() for song in song_name_spans]
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    song_name_spans = soup.select('li ul li h3')
-    song_names = [song.getText().strip() for song in song_name_spans]
-    all_song_names.extend(song_names)
+        all_song_names.extend(song_names)
 
-    artist_spans = soup.find_all(name="span", class_="a-no-trucate")
-    artist_names = [artist.getText().strip() for artist in artist_spans]
-    all_artist_names.extend(artist_names)
+        artist_spans = soup.find_all(name="span", class_="a-no-trucate")
+        artist_names = [artist.getText().strip().replace('Featuring', 'ft.')
+                        if 'Featuring' in artist.getText().strip()
+                        else artist.getText().strip() for artist in artist_spans]
+        all_artist_names.extend(artist_names)
 
-    month += 1
-
-print(len(all_artist_names))
+        month += 1
+    year += 1
+# Use a dictionary to handle duplicate song names, get rid of value associated with it
+track_dict = dict(zip(all_song_names, all_artist_names))
+all_artist_names = list(track_dict.keys())
+all_song_names = list(track_dict.values())
+# [Testing] Check song + artist matches
+print(str(len(all_song_names)) + " / " + str(len(all_artist_names)))
 
 sp = spotipy.Spotify(
     auth_manager = SpotifyOAuth(
@@ -48,16 +57,21 @@ sp = spotipy.Spotify(
 )
 user_id = sp.current_user()['id']
 song_uris = []
-print(len(song_uris))
+
+# # [Testing] Check initial song matches after dictionary dupe handling
+# for item in range(0, len(all_song_names)):
+#     print(all_song_names[item] + " - " + all_artist_names[item] + " ")
 
 for item in range(0, len(all_song_names)):
     if item % 100 == 0:
         print("Milestone: " + str(item))
         time.sleep(30)
         print("Sleep done") # workaround for Spotify API rate limit
+
+    result = sp.search(q=all_artist_names[item] + "-" + all_song_names[item], type = "track")
+    # [Testing] Check song/artist match with result query
     # print(all_song_names[item] + " - " + all_artist_names[item])
     # print(all_artist_names[item] + " - " + all_song_names[item])
-    result = sp.search(q=all_artist_names[item] + "-" + all_song_names[item], type = "track")
     # print(str(result["tracks"]["items"][0]["artists"][0]["name"]) + " - " + str(result["tracks"]["items"][0]['name']))
 
     try:
@@ -67,17 +81,22 @@ for item in range(0, len(all_song_names)):
         print(f"{all_song_names[item]} doesnt exist on Spotify")
 
 print("Done Searching")
-playlist = sp.user_playlist_create(user = user_id, name=f"{year} Test Billboard 100", public=False)
+# Create the playlist
+playlist = sp.user_playlist_create(user = user_id, name=f"2010s Test Billboard 100", public=False)
 
+# API handling - split the array into chunks for playlist adding
 floor = 0
 ceiling = 100
 loop_num = 0
+
 if len(all_song_names) % 100 == 0:
     loop_num = int(len(all_song_names)/100)
-else: #handle the remainders
+else: # handle the remainders
     loop_num = int(len(all_song_names)/100) + 1
 # work around for Spotify API rate limit, break adding songs from array req to every 100
 for i in range(0, loop_num):
+    print("Loop MS: " + str(i))
+    # upload songs to the playlist
     sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris[floor:ceiling])
     floor = ceiling
     if len(all_song_names) - ceiling < 100: # when there are dupes, len may not be 1200
